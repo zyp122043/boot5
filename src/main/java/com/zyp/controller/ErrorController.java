283f4023d829ce178f006225e39decdb3559ffd5
weixin_global <987148165@qq.com>
2020-05-14 10:23:30 +0800

    03

---
+++ src/main/java/com/zyp/controller/ErrorController.java
@@ -0,0 +1,12 @@
+package com.zyp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ErrorController {
+    @RequestMapping("/error01")
+    public String error() {
+        return "error02";
+    }
+}
