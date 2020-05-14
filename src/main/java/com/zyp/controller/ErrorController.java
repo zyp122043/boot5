020bbb25cba84e63d3272c3516de12516fa09ce4
weixin_global <987148165@qq.com>
2020-05-14 11:02:04 +0800

    2006

--- src/main/java/com/zyp/controller/ErrorController.java
+++ src/main/java/com/zyp/controller/ErrorController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ErrorController {
     @RequestMapping("/error01")
     public String error() {
-        return "error04";
+        return "error05";
     }
 }
